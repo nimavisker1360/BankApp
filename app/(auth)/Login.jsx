a4e34d3e08594c06afabb29917c9813354e568cd
nimavisker1360 <nimavisker@gmail.com>
2025-03-15 19:21:19 +0300

    Add Login Account Template

--- app/(auth)/Login.jsx
+++ app/(auth)/Login.jsx
@@ -1,13 +1,68 @@
-import React from 'react'
-import { View,Text } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import React from "react";
+
+import { SafeAreaView } from "react-native-safe-area-context";
+import { icons } from "../../constants";
+import { useState } from "react";
+import { Link } from "expo-router";
+import {
+  Image,
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Platform,
+} from "react-native";
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
   return (
-    <SafeAreaView>
-        <Text>Log In</Text>
-    </SafeAreaView>
-  )
-}
+    <SafeAreaView className="bg-primary h-full w-full justify-center px-3">
+      <View className="w-full justify-center items-center mt-7">
+        <Text className="text-2xl font-pbold text-secondary">Welcome</Text>
+        <Text className="text-gray-200 text-lg">Login to get Started</Text>
+      </View>
 
-export default Login
+      <View className="mt-6 rounded-3xl border-2 border-[#E7E7E7] flex-row items-center w-full h-[56px] px-4">
+        <Image className="w-6 h-6" source={icons.phone} resizeMode="contain" />
+        <TextInput
+          className="flex-1 font-pmedium ml-2"
+          placeholder="Phone number"
+          keyboardType="number-pad"
+        />
+      </View>
+
+      <View className="mt-6 rounded-3xl border-2 border-[#E7E7E7] flex-row items-center w-full h-[56px] px-4">
+        <Image className="w-6 h-6" source={icons.lock} resizeMode="contain" />
+        <TextInput
+          className="flex-1 font-pmedium ml-2"
+          placeholder="Password"
+          secureTextEntry={!showPassword}
+        />
+        <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+          <Image
+            className="w-6 h-6"
+            source={!showPassword ? icons.eye : icons.eyeHide}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity className="bg-secondary mt-5 flex-row p-3 rounded-full items-center justify-center">
+        <Text className="ml-3 text-lg text-white items-center justify-center">
+          Submit
+        </Text>
+      </TouchableOpacity>
+
+      <View className="w-full justify-end items-center pt-3 flex-row">
+        <Text className="font-pregular text-gray-200">
+          Don't have an account?
+        </Text>
+        <Link href="/Login" className="text-lg text-secondary mx-2">
+          Register
+        </Link>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Login;
--- app/(auth)/Register.jsx
+++ app/(auth)/Register.jsx
@@ -11,6 +11,8 @@ import {
 import DateTimePicker from "@react-native-community/datetimepicker";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { icons } from "../../constants";
+import {Link} from "expo-router";
+import Checkbox from "expo-checkbox";
 
 const Register = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -57,40 +59,73 @@ const Register = () => {
 
         {/* Phone Input */}
         <View className="mt-6 rounded-3xl border-2 border-[#E7E7E7] flex-row items-center w-full h-[56px] px-4">
-          <Image className="w-6 h-6" source={icons.phone} resizeMode="contain" />
-          <TextInput className="flex-1 font-pmedium ml-2" placeholder="Phone number" keyboardType="number-pad" />
+          <Image
+            className="w-6 h-6"
+            source={icons.phone}
+            resizeMode="contain"
+          />
+          <TextInput
+            className="flex-1 font-pmedium ml-2"
+            placeholder="Phone number"
+            keyboardType="number-pad"
+          />
         </View>
 
         {/* Email Input */}
         <View className="mt-6 rounded-3xl border-2 border-[#E7E7E7] flex-row items-center w-full h-[56px] px-4">
           <Image className="w-6 h-6" source={icons.mail} resizeMode="contain" />
-          <TextInput className="flex-1 font-pmedium ml-2" placeholder="Email Address" keyboardType="email-address" />
+          <TextInput
+            className="flex-1 font-pmedium ml-2"
+            placeholder="Email Address"
+            keyboardType="email-address"
+          />
         </View>
 
         {/* Password Input */}
         <View className="mt-6 rounded-3xl border-2 border-[#E7E7E7] flex-row items-center w-full h-[56px] px-4">
           <Image className="w-6 h-6" source={icons.lock} resizeMode="contain" />
-          <TextInput className="flex-1 font-pmedium ml-2" placeholder="Password" secureTextEntry={!showPassword} />
+          <TextInput
+            className="flex-1 font-pmedium ml-2"
+            placeholder="Password"
+            secureTextEntry={!showPassword}
+          />
           <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
-            <Image className="w-6 h-6" source={!showPassword ? icons.eye : icons.eyeHide} resizeMode="contain" />
+            <Image
+              className="w-6 h-6"
+              source={!showPassword ? icons.eye : icons.eyeHide}
+              resizeMode="contain"
+            />
           </TouchableOpacity>
         </View>
 
         {/* First Name */}
         <View className="mt-6 rounded-3xl border-2 border-[#E7E7E7] flex-row items-center w-full h-[56px] px-4">
           <Image className="w-6 h-6" source={icons.user} resizeMode="contain" />
-          <TextInput className="flex-1 font-pmedium ml-2" placeholder="First Name" />
+          <TextInput
+            className="flex-1 font-pmedium ml-2"
+            placeholder="First Name"
+          />
         </View>
 
         {/* Last Name */}
         <View className="mt-6 rounded-3xl border-2 border-[#E7E7E7] flex-row items-center w-full h-[56px] px-4">
           <Image className="w-6 h-6" source={icons.user} resizeMode="contain" />
-          <TextInput className="flex-1 font-pmedium ml-2" placeholder="Last Name" />
+          <TextInput
+            className="flex-1 font-pmedium ml-2"
+            placeholder="Last Name"
+          />
         </View>
 
         {/* Date Picker */}
-        <TouchableOpacity onPress={() => setShowDatePicker(true)} className="mt-6 rounded-3xl border-2 border-[#E7E7E7] flex-row items-center w-full h-[56px] px-4">
-          <Image className="w-6 h-6" source={icons.calendar} resizeMode="contain" />
+        <TouchableOpacity
+          onPress={() => setShowDatePicker(true)}
+          className="mt-6 rounded-3xl border-2 border-[#E7E7E7] flex-row items-center w-full h-[56px] px-4"
+        >
+          <Image
+            className="w-6 h-6"
+            source={icons.calendar}
+            resizeMode="contain"
+          />
           <Text className="flex-1 text-gray-300 ml-2">
             {form.dateOfBirth || "Select Date of Birth"}
           </Text>
@@ -104,6 +139,25 @@ const Register = () => {
             onChange={onChange}
           />
         )}
+        <View className="flex-row items-center w-full h-[56px] px-4">
+          <Checkbox value={checked} onValueChange={setChecked} />
+          <Text className="ml-3">I agree to the terms and conditions</Text>
+        </View>
+
+        <TouchableOpacity className="bg-secondary mt-5 flex-row p-3 rounded-full items-center justify-center">
+          <Text className="ml-3 text-lg text-white items-center justify-center">
+            Submit
+          </Text>
+        </TouchableOpacity>
+
+        <View className="w-full justify-end items-center pt-3 flex-row">
+          <Text className="font-pregular text-gray-200">
+            Already have an account?{" "}
+          </Text>
+          <Link href="/Login" className="text-lg text-secondary mx-2">
+            Login
+          </Link>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
