f5f4fd7c32cfd00f75e91f4e2711d970d664a7a2
nimavisker1360 <nimavisker@gmail.com>
2025-04-04 12:05:36 +0300

    Add otp

--- app/(auth)/Login.jsx
+++ app/(auth)/Login.jsx
@@ -13,12 +13,9 @@ import {
 import React, { useState, useEffect } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { router } from "expo-router";
-import Checkbox from "expo-checkbox";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [rememberMe, setRememberMe] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState("");
   const [keyboardVisible, setKeyboardVisible] = useState(false);
 
   useEffect(() => {
@@ -48,129 +45,65 @@ const Login = () => {
         className="flex-1"
         keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
       >
-        <ScrollView
-          contentContainerStyle={{
-            flexGrow: 1,
-            justifyContent: keyboardVisible ? "flex-start" : "center",
-          }}
-          keyboardShouldPersistTaps="handled"
-          className="p-5"
-        >
+        <View className="flex-1 px-5">
           <View className="absolute top-10 left-5 z-10">
-            <TouchableOpacity onPress={() => router.push("/(auth)/Register")}>
+            <TouchableOpacity onPress={() => router.push("/Register")}>
               <Ionicons name="arrow-back" size={24} color="black" />
             </TouchableOpacity>
           </View>
 
-          <View
-            className={`flex-1 items-center ${
-              keyboardVisible ? "justify-start pt-20" : "justify-center"
-            }`}
-          >
-            <View
-              className={`items-center ${keyboardVisible ? "mb-5" : "mb-10"} ${
-                keyboardVisible ? "mt-5" : "mt-16"
-              }`}
-            >
-              <Text className="text-3xl font-bold">Login to your account</Text>
-            </View>
-
-            <View className="w-full space-y-4 mb-2">
-              <View className="flex-row items-center p-3 rounded-xl bg-gray-50">
-                <Ionicons name="mail" size={22} color="gray" />
-                <TextInput
-                  className="flex-1 text-base ml-3"
-                  placeholder="Email"
-                  value={email}
-                  onChangeText={setEmail}
-                  keyboardType="email-address"
-                  autoCapitalize="none"
-                />
-              </View>
-
-              <View className="flex-row items-center p-3 rounded-xl bg-gray-50">
-                <Ionicons name="lock-closed" size={22} color="gray" />
-                <TextInput
-                  className="flex-1 text-base ml-3"
-                  placeholder="Password"
-                  value={password}
-                  onChangeText={setPassword}
-                  secureTextEntry
-                />
-              </View>
-            </View>
-
-            <View className="flex-row items-center w-full mb-4 mt-2">
-              <Checkbox
-                value={rememberMe}
-                onValueChange={setRememberMe}
-                color={rememberMe ? "#4285F4" : undefined}
-                className="h-5 w-5 rounded-sm"
-              />
-              <Text className=" ml-2 text-[12px]">Remember me</Text>
-            </View>
-
-            <TouchableOpacity
-              className="w-full bg-[#4285F4] py-3 rounded-full items-center mb-3"
-              onPress={() => router.push("/(tabs)/Home")}
-            >
-              <Text className="text-white text-lg font-semibold">Login</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-              className="mb-4 items-center"
-              onPress={() => router.push("/(auth)/ForgotPassword")}
-            >
-              <Text className="text-[#4285F4] font-medium">
-                Forgot Password?
-              </Text>
-            </TouchableOpacity>
-
-            <Text className={`text-lg ${keyboardVisible ? "mb-3" : "mb-6"}`}>
-              or continue with
+          <View className="items-center mt-24 mb-12">
+            <Text className="text-3xl font-bold text-center text-[#1E1E1E]">
+              Log in
             </Text>
-
-            <View
-              className={`flex-row w-full justify-evenly ${
-                keyboardVisible ? "mb-5" : "mb-10"
-              }`}
-            >
-              <TouchableOpacity className="w-24 h-14 border border-gray-100 rounded-xl items-center justify-center">
-                <Ionicons
-                  name="logo-apple"
-                  size={30}
-                  color="black"
-                  onPress={() => router.push("/(auth)/Experience")}
-                />
-              </TouchableOpacity>
-
-              <TouchableOpacity className="w-24 h-14 border border-gray-100 rounded-xl items-center justify-center">
-                <Ionicons name="logo-facebook" size={30} color="#4267B2" />
-              </TouchableOpacity>
-
-              <TouchableOpacity className="w-24 h-14 border border-gray-100 rounded-xl items-center justify-center">
-                <Image
-                  source={{
-                    uri: "https://www.gstatic.com/images/branding/product/2x/googleg_48dp.png",
-                  }}
-                  className="w-8 h-8"
-                  resizeMode="contain"
-                />
-              </TouchableOpacity>
-            </View>
-
-            <View className="flex-row">
-              <Text className="text-base text-gray-700">
-                Don't have an account?{" "}
-              </Text>
-              <TouchableOpacity onPress={() => router.push("/(auth)/Signup")}>
-                <Text className="text-base text-[#4285F4] font-medium">
-                  Sign Up
-                </Text>
-              </TouchableOpacity>
-            </View>
+            <Text className="text-center text-[#7D7D7D] mt-4">
+              Login with your  email address or Google account
+            </Text>
+           
           </View>
-        </ScrollView>
+
+          <View className="w-full mb-28">
+            <TextInput
+              className="w-full p-4 text-base border border-gray-100 rounded-md bg-white"
+              placeholder="Email address or phone number"
+              value={phoneNumber}
+              onChangeText={setPhoneNumber}
+              keyboardType="phone-pad"
+            />
+          </View>
+
+          <TouchableOpacity
+            className={`self-center ${keyboardVisible ? "mb-8" : "mb-20"}`}
+          >
+            <Text className="text-base text-[#1E1E1E]">Having trouble?</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            className="w-full bg-[#E5E9EC] py-4 rounded-md items-center mb-12"
+            onPress={() => router.push("/(auth)/Experience")}
+          >
+            <Text className="text-[#1E1E1E] text-lg font-medium">Continue</Text>
+          </TouchableOpacity>
+
+          <View className="flex-row items-center justify-center mb-12">
+            <View className="h-[1px] bg-gray-200 flex-1" />
+            <Text className="text-[#7D7D7D] mx-4">or</Text>
+            <View className="h-[1px] bg-gray-200 flex-1" />
+          </View>
+
+          <TouchableOpacity className="w-full bg-[#F0483E] py-4 rounded-md items-center flex-row justify-center">
+            <Image
+              source={{
+                uri: "https://img.icons8.com/color/48/000000/google-logo.png",
+              }}
+              style={{ width: 24, height: 24 }}
+              className="mr-3"
+            />
+            <Text className="text-white text-lg font-medium">
+              Register with Google
+            </Text>
+          </TouchableOpacity>
+        </View>
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
--- app/(auth)/Signup.jsx
+++ app/(auth)/Signup.jsx
@@ -45,32 +45,26 @@ const Signup = () => {
         className="flex-1"
         keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
       >
-        <ScrollView
-          contentContainerStyle={{
-            flexGrow: 1,
-            justifyContent: keyboardVisible ? "flex-start" : "center",
-            paddingHorizontal: 20,
-          }}
-          keyboardShouldPersistTaps="handled"
-        >
+        <View className="flex-1 px-5">
           <View className="absolute top-10 left-5 z-10">
-            <TouchableOpacity onPress={() => router.back()}>
+            <TouchableOpacity onPress={() => router.push("/Register")}>
               <Ionicons name="arrow-back" size={24} color="black" />
             </TouchableOpacity>
           </View>
 
-          <View className="items-center mt-16 mb-8">
+          <View className="items-center mt-24 mb-12">
             <Text className="text-3xl font-bold text-center text-[#1E1E1E]">
               Create an Account
             </Text>
-            <Text className="text-center text-[#7D7D7D] mt-2">
-              Register with your phone number, Google, or Facebook
+            <Text className="text-center text-[#7D7D7D] mt-4">
+              Register with your phone number, Google,
             </Text>
+            <Text className="text-center text-[#7D7D7D]">or Facebook</Text>
           </View>
 
-          <View className="w-full mb-8">
+          <View className="w-full mb-28">
             <TextInput
-              className="w-full p-3 text-base border border-gray-200 rounded-xl bg-white"
+              className="w-full p-4 text-base border border-gray-200 rounded-md bg-white"
               placeholder="Phone number"
               value={phoneNumber}
               onChangeText={setPhoneNumber}
@@ -78,32 +72,38 @@ const Signup = () => {
             />
           </View>
 
-          <TouchableOpacity className="self-center mb-8">
+          <TouchableOpacity
+            className={`self-center ${keyboardVisible ? "mb-8" : "mb-20"}`}
+          >
             <Text className="text-base text-[#1E1E1E]">Having trouble?</Text>
           </TouchableOpacity>
 
           <TouchableOpacity
-            className="w-full bg-[#E5E9EC] py-3 rounded-xl items-center mb-6"
+            className="w-full bg-[#E5E9EC] py-4 rounded-md items-center mb-12"
             onPress={() => router.push("/(auth)/Experience")}
           >
             <Text className="text-[#1E1E1E] text-lg font-medium">Continue</Text>
           </TouchableOpacity>
 
-          <View className="flex-row items-center justify-center mb-6">
+          <View className="flex-row items-center justify-center mb-12">
             <View className="h-[1px] bg-gray-200 flex-1" />
             <Text className="text-[#7D7D7D] mx-4">or</Text>
             <View className="h-[1px] bg-gray-200 flex-1" />
           </View>
 
-          <TouchableOpacity className="w-full bg-[#F0483E] py-3 rounded-xl items-center flex-row justify-center">
-            
-              <Text className="text-xl font-bold mr-12 text-[#Ffff]">G</Text>
-        
+          <TouchableOpacity className="w-full bg-[#F0483E] py-4 rounded-md items-center flex-row justify-center">
+            <Image
+              source={{
+                uri: "https://img.icons8.com/color/48/000000/google-logo.png",
+              }}
+              style={{ width: 24, height: 24 }}
+              className="mr-3"
+            />
             <Text className="text-white text-lg font-medium">
               Register with Google
             </Text>
           </TouchableOpacity>
-        </ScrollView>
+        </View>
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
