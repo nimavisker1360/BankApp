8c7163e9ffdf643fa235c323cb5366c7da4dbe24
nimavisker1360 <nimavisker@gmail.com>
2025-04-04 13:32:42 +0300

    Ready for Register

--- app/(auth)/Login.jsx
+++ app/(auth)/Login.jsx
@@ -80,7 +80,7 @@ const Login = () => {
 
           <TouchableOpacity
             className="w-full bg-[#E5E9EC] py-4 rounded-md items-center mb-12"
-            onPress={() => router.push("/(auth)/Experience")}
+            onPress={() => router.push("/(tabs)/Home")}
           >
             <Text className="text-[#1E1E1E] text-lg font-medium">Continue</Text>
           </TouchableOpacity>
--- app/(auth)/Signup.jsx
+++ app/(auth)/Signup.jsx
@@ -80,7 +80,7 @@ const Signup = () => {
 
           <TouchableOpacity
             className="w-full bg-[#E5E9EC] py-4 rounded-md items-center mb-12"
-            onPress={() => router.push("/(auth)/Experience")}
+            onPress={() => router.push("/(verify)/NewPin")}
           >
             <Text className="text-[#1E1E1E] text-lg font-medium">Continue</Text>
           </TouchableOpacity>
